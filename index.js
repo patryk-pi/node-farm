830f5c263dde4b35fb1951e9cb2a048f28419977
patryk-pi <patryk.piasecki1@gmail.com>
2023-06-23 12:38:05 +0200

    filling the templates

--- index.js
+++ index.js
@@ -40,23 +40,70 @@ console.log('File written!')*/
 
 /////////////////////////////////////////// SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%ORIGIN%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID}/g, product.id);
+
+    if (!product.organic)
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+    return output;
+};
+
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template_overview.html`,
+    "utf-8"
+);
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template_card.html`,
+    "utf-8"
+);
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template_product.html`,
+    "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview page
+
     if (pathName === "/" || pathName === "/overview") {
-        res.end("This is an overview");
+        res.writeHead(200, { "Content-type": "text/html" });
+
+        const cardsHtml = dataObj
+            .map((el) => replaceTemplate(tempCard, el))
+            .join("");
+
+        const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+        res.end(output);
+
+        // Product page
     } else if (pathName === "/product") {
         res.end("This is a product!");
+
+        // API
     } else if (pathName === "/api") {
         // fs.readFile("./dev-data/data.json");
 
-        res.writeHead(200, { "Content-type": "application/json" });
+        res.writeHead(200, {
+            "Content-type": "application/json",
+        });
         res.end(data);
 
         // res.end("This is the PRODUCT");
+
+        // NOT FOUND
     } else {
         res.writeHead(404);
         res.end("Page not found!");
